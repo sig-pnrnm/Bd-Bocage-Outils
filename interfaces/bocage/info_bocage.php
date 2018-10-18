36b1a61c93f0a6e602e7e6683c6b09eb15ddf3d4
Sylvain Montagner <sig-pnrnm@users.noreply.github.com>
2018-10-18 11:12:17 +0200

    Create info_bocage.php

---
+++ interfaces/bocage/info_bocage.php
@@ -0,0 +1,169 @@
+<div class="modal fade" id="aboutModalMap" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    
+      <div class="modal-header">
+        <button class="close" type="button" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Observatoire de l'évolution du Bocage</h4>
+      </div>
+      
+      <div class="modal-body">
+      
+        <ul class="nav nav-tabs nav-justified" id="aboutTabs">
+          <li class="active">
+            <a href="#aboutbocage" data-toggle="tab"><i class="fa fa-question-circle"></i>&nbsp;Le Bocage</a>
+          </li>
+          <li>
+            <a href="#disclaimerbocage" data-toggle="tab"><i class="fa fa-exclamation-circle"></i>&nbsp;Informations et méthode</a>
+          </li>
+          <li>
+            <a href="#metadonnees" data-toggle="tab"><i class="fa fa-globe"></i>&nbsp;Métadonnées</a>
+          </li>
+        </ul>
+        
+        <div class="tab-content" id="aboutTabsContent">
+          <div class="tab-pane fade active in" id="aboutbocage">
+
+            <figure class="figure" style="float: right">
+              <img src="bocage/bocage_houyvet_500px.jpg" style="width:350px; margin: 10px;" class="figure-img img-fluid rounded img-thumbnail" alt="Bocage">
+              <figcaption class="figure-caption text-right"><small>© J.Houyvet / <a href="http://lumieresmarines.com" target="_blank">lumieresmarines.com</a></small></figcaption>
+            </figure>
+
+          <p>
+          <br>
+          Le «&nbsp;<a href="https://fr.wikipedia.org/wiki/Bocage" target="_blank" data-toggle="tooltip" data-placement="top" title="Définition Wikipedia">Bocage</a>&nbsp;» est un élément constitutif et identitaire du Parc naturel régional Normandie-Maine qui œuvre depuis sa création à sa préservation et sa restauration.<br>
+          </p>
+          <p>
+          Cette carte présente l’évolution de la densité bocagère qui se définit par le ratio entre le linéaire de haies et la surface considérée. Ce ratio constitue un bon indicateur de l’état du bocage.
+          </p>
+          <p>
+          Partout en France, il a connu un fort recul depuis la moitié du 20ème siècle, notamment en raison des remembrements et de la modernisation agricole.<br>
+          </p>
+          <p>
+          Pourtant, les haies rendent de nombreux services (production de bois et de fruits, lutte contre l’érosion, abri pour la faune, effet brise vent, …) et les paysages de bocage constituent un atout en terme d’attractivité résidentielle et touristique.
+          </p>
+            <div class="alert alert-success" role="alert">
+              <a href="http://observatoire.parc-naturel-normandie-maine.fr/bocage_diachronique.php" title="Interface diachronique">
+                <img src="bocage/interface_bocage_diachro_200px.png" style="width:200px; margin-right: 10px; margin-top: -10px; float: left" class="figure-img img-fluid rounded img-thumbnail" alt="Interface densité">
+              </a>
+              En complément de cette carte statistique, une autre carte interactive permet de comparer de manière synchronisée le linéaire de haies de 1950 et 2010, sur la base des clichés aériens de l’IGN.<br>
+              L’internaute peut ainsi visualiser l’importance du réseau de haies de son territoire.<br>
+              <a href="http://observatoire.parc-naturel-normandie-maine.fr/bocage_diachronique.php" class="btn btn-success btn-xs" role="button" title="Interface diachronique">
+                <span class="glyphicon glyphicon-link"></span>&nbsp;&nbsp;Interface diachronique
+              </a>
+            </div>
+            <div>
+              <a href="http://www.parc-naturel-normandie-maine.fr" class="btn btn-success btn-xs" role="button" title="En savoir + sur l'action du Parc">
+                <span class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;L'action du Parc
+              </a>
+              <a href="http://www.parc-naturel-normandie-maine.fr/agir/observatoire.html" class="btn btn-success btn-xs" role="button" title="En savoir + sur l'Observatoire">
+                <span class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;L'Observatoire
+              </a>
+              <a href="mailto:communication@parc-normandie-maine.fr?subject=[Interfaces Bocage]" class="btn btn-success btn-xs" role="button" target="_blank" title="Nous contacter">
+                <span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Nous contacter
+              </a>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="disclaimerbocage">
+            <br>
+            <br>
+            <div class="alert alert-success" role="alert">
+              <h3>La méthode Parc</h3>
+              <p>
+              Les haies ont été cartographiées par photo-interprétation [1] sur les clichés aériens de l’IGN (Bd-Ortho) disponibles permettant de constituer une base de données de l’évolution du linéaire de haies depuis les années 50 (approche diachronique).<br>
+              Cette base de données permet d’analyser la structure du réseau de haies au travers de différents indicateurs (densité, cohérence, ...), et ce à une échelle très fine.<br><br>
+              <small>[1] La photo-interprétation est un processus manuel de reconnaissance d’objets cartographiques (ici les haies), par opposition à la télédétection qui consiste en un processus automatisé. La bibliographie sur le sujet permettait de conclure (au moins en 2011) que la reconnaissance automatisée des haies sur photos aériennes souffrait de biais trop importants. La marge d’erreur des données a été estimée à moins de 5% (surévaluation 1%, sous évaluation 5%).</small>
+              </p>
+              
+              <h3>Un inventaire mutualisé en &laquo;inter-parcs&raquo; Normands</h3>
+              <div class="row">
+                <div class="col-xs-6 col-md-4">
+                  <p>
+                  Ce travail d'inventaire a été réalisé en partenariat avec les autres PNR Normands qui sont tous concernés par la thématique bocagère.
+                  </p>
+                </div>
+                <div class="col-xs-6 col-md-4">
+                  <img src="http://www.gmn.asso.fr/public/PIAL_2014-2018/.Logo_PNRNM_s.jpg" title="PNR Normandie-Maine" class="img-thumbnail" alt="PNRNM" width="50" height="50">
+                  <img src="http://www.pnr-seine-normande.com/img/logo.gif" title="PNR des Boucles de la Seine-Normandie" class="img-thumbnail" alt="PNRBSN" width="50" height="50">
+                  <img src="http://www.naturefrance.fr/sites/default/files/styles/190large/public/fichiers/images/structure/parc_naturel_regional_des_marais_du_cotentin_et_du_bessin/logo-pnrmcb-couleur.jpg" title="PNR des Marais du Cotentin et du Bessin" class="img-thumbnail" alt="PNRMCB" width="50" height="50">
+                  <img src="http://www.parc-naturel-perche.fr/sites/pnr-perche/themes/theme_base/logo.png" title="PNR du Perche" class="img-thumbnail" alt="PNRP" width="50" height="50">
+                </div>
+              </div>
+            </div>
+            <div class="alert alert-danger" role="alert">
+              <p><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;&nbsp;Les données de cette carte sont fournies uniquement à des fins d'information et de planification.</p>
+              <p>Aucune garantie de précision ou d'exhaustivité n'est prévue.</p>
+              <p>Certaines informations de cette carte peuvent être soumise à droits d'auteur.</p>
+            </div>
+          </div>
+
+
+          <div class="tab-pane fade" id="metadonnees">
+            <br>
+            <div class="alert alert-info" role="alert">
+              <h3>Bd-Bocage</h3>
+              <p>&copy; <a href="http://www.parc-naturel-normandie-maine.fr">Parc Normandie-Maine</a>, 2014. Photo-interprétation : <a href="http://www.memoris.fr">Mémoris</a></p>
+              <p>La «&nbsp;<b>Bd-Bocage</b>&nbsp;» ou <b>référentiel SIG du Bocage</b> du Parc naturel régional Normandie-Maine est une base de données des haies numérisées sur l’ensemble du territoire du Parc sur trois années de référence : 1945, 2000 et 2010*.<br>
+              Elle permet d'apporter une vision diachronique et localisée de l’évolution du bocage pour dégager des priorités d’actions du Parc et comprendre l’évolution des paysages<br>
+              <br>
+              <i>* détail des années de prises de vues aériennes (BD ORTHO® Historique IGN) utilisées pour la numérisation des haies :</i>
+              <div style="width:50%;">
+                <table class="table table-sm" style='font-size:90%'>
+                <thead class="thead-light">
+                <tr>
+                  <th scope="col"></th>
+                  <th scope="col">1950</th>
+                  <th scope="col">2000</th>
+                  <th scope="col">2010</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  <th scope="row">50 - Manche</th>
+                  <td>1947</td>
+                  <td>2002</td>
+                  <td>2010</td>
+                  </tr>
+                  <tr>
+                  <th scope="row">53 - Mayenne</th>
+                  <td>1950</td>
+                  <td>2001</td>
+                  <td>2010</td>
+                  </tr>
+                  <tr>
+                  <th scope="row">61 - Orne</th>
+                  <td>1947</td>
+                  <td>2001</td>
+                  <td>2010</td>
+                  </tr>
+                  <tr>
+                  <th scope="row">72 - Sarthe</th>
+                  <td>1950</td>
+                  <td>2000</td>
+                  <td>2010</td>
+                  </tr>
+                </tbody>
+                </table>
+              </div>
+              </p>
+              <br>
+              <a href="http://metadata.carmencarto.fr/geosource/139/fre/find?uuid=c0b788e5-1a4c-4ec0-9041-9523e282972d" class="btn btn-default btn-xs" role="button" data-toggle="tooltip" data-placement="top" title="Métadonnées officielles sur GeoSource"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Fiche métadonnées</a>
+            </div>
+            
+            <div class="alert alert-info" role="alert">
+              <h3>IGN Bd-GeoFLA</h3>
+              <p>Description de l'ensemble des unités administratives nationales (France métropolitaine et DROM) : communes, cantons, arrondissements, départements, régions.<br>
+              GEOFLA® permet à des échelles nationales et régionales de situer toute information thématique, d'analyser des données statistiques et de gérer des déplacements routiers.<br>
+              <a href="http://professionnels.ign.fr/geofla" class="btn btn-default btn-xs" role="button" data-toggle="tooltip" data-placement="top" title="Métadonnées officielles"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Fiche métadonnées</a>
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
