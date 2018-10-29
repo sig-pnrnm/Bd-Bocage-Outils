daf49608f0b48496ad1df206156fe4fe9d6087f3
Sylvain Montagner <sig-pnrnm@users.noreply.github.com>
2018-10-29 11:36:28 +0100

    Create help_bocage_densite.php

---
+++ interfaces/bocage/help_bocage_densite.php
@@ -0,0 +1,61 @@
+    <div class="modal fade" id="aboutModalHelp" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button class="close" type="button" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">
+                <i class="fa fa-question-circle text-info" aria-hidden="true" style="display: inline-block; line-height:50px; font-size: 30px;"></i>
+                Aide sur cette interface
+            </h4>
+          </div>
+          <div class="modal-body">
+            <div class="tab-content" id="aboutTabsContent">
+
+                <p>Découvrez toutes les fonctionnalités des cartes grâce aux tutoriels vidéos ci-dessous.</p>
+
+                <div class="row">
+                  <div class="col-xs-6">
+                    <h4>Chercher une commune</h4>
+                    <div style="float: left; margin: 10px;" >
+                    <a href="bocage/bocage_tuto_commune.gif" onclick="window.open(this.href, 'Tutoriel', 'height=670, width=1000, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no'); return(false);" title="Tutoriel date" target="_blank"><img src="bocage/bocage_communes_lecteur.png" title="Aide communes" class="img-thumbnail" alt="Aide" width="200" height="135"></a>
+                    </div>
+                    <p>Vous pouvez focaliser l'interface sur une commune du Parc pour consulter ses statistiques locales.<br>
+                    Le moteur de recherche zoome directement sur la commune.<p>
+                  </div>
+                  <div class="col-xs-6">
+                    <h4>Changer la date d'analyse</h4>
+                    <div style="float: left; margin: 10px;" >
+                    <a href="bocage/bocage_tuto_dates.gif" onclick="window.open(this.href, 'Tutoriel', 'height=670, width=1000, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no'); return(false);" title="Tutoriel date" target="_blank"><img src="bocage/bocage_date_lecteur.png" title="Aide date" class="img-thumbnail" alt="Aide" width="200" height="135"></a>
+                    </div>
+                    <p>La carte chargée initialement représente la densité bocagère en 2010.<br>
+                    Vous pouvez changer cette date ou afficher une carte de l'évolution de la densité entre 1945 et 2010.<p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-6">
+                    <h4>Changer l'échelle d'analyse</h4>
+                    <div style="float: left; margin: 10px;" >
+                    <a href="bocage/bocage_tuto_mailles.gif" onclick="window.open(this.href, 'Tutoriel', 'height=670, width=1000, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no'); return(false);" title="Tutoriel date" target="_blank"><img src="bocage/bocage_mailles_lecteur.png" title="Aide échelle" class="img-thumbnail" alt="Aide" width="200" height="135"></a>
+                    </div>
+                    <p>Par défaut, les données s'affichent à l'échelle communale (communes nouvelles).<br>Vous pouvez changer l'échelle et consulter les statistiques des anciennes communes (avant fusion), des EPCI ou des départements.<br>
+                    Il est également possible d'afficher les statistiques par kilomètre carré (maillage de 1km²).<p>
+                  </div>
+                  <div class="col-xs-6">
+                    <h4>Changer les fonds de cartes</h4>
+                    <div style="float: left; margin: 10px;" >
+                    <a href="bocage/bocage_tuto_fonds.gif" onclick="window.open(this.href, 'Tutoriel', 'height=670, width=1000, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no'); return(false);" title="Tutoriel date" target="_blank"><img src="bocage/bocage_fonds_lecteur.png" title="Aide fonds" class="img-thumbnail" alt="Aide" width="200" height="135"></a>
+                    </div>
+                    <p>Par défaut, un fond de carte noir et blanc est affiché.<br>
+                    Vous pouvez basculer sur différents fonds tels que les photographies aériennes actuelles ou historiques de l'IGN.<br>
+                    Vous pouvez également afficher ou masquer d'autres couches de données : haies, périmètre Parc, densité...<p>
+                  </div>
+                </div>
+
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
